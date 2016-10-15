b39ba226edd81eb19d259a8b4ca0188e9070eab8
kortes <sergey@4-7i.ru>
2016-10-15 20:52:41 +0500

    start map

--- src/js/main.js
+++ src/js/main.js
@@ -73,4 +73,35 @@ $(document).ready(function() {
 	        myMap.geoObjects.add(myPlacemark);
 	    }
 	}
+
+	// Определим свои функции добавления/удаления класса, так как те, что в jQuery не работают для SVG
+	jQuery.fn.myAddClass = function (classTitle) {
+	  return this.each(function() {
+	    var oldClass = jQuery(this).attr("class");
+	    oldClass = oldClass ? oldClass : '';
+	    jQuery(this).attr("class", (oldClass+" "+classTitle).trim());
+	  });
+	}
+	jQuery.fn.myRemoveClass = function (classTitle) {
+	  return this.each(function() {
+	      var oldClass = jQuery(this).attr("class");
+	      var startpos = oldClass.indexOf(classTitle);
+	      var endpos = startpos + classTitle.length;
+	      var newClass = oldClass.substring(0, startpos).trim() + " " + oldClass.substring(endpos).trim();
+	      if (!newClass.trim())
+	        jQuery(this).removeAttr("class");
+	      else
+	        jQuery(this).attr("class", newClass.trim());
+	  });
+	}
+
+	if ($(".map").length > 0){
+		$(".map__legend-item").hover(function(){
+			var self = $(this);
+			$(".map__shop_type_" + self.data("type")).myAddClass("map__shop_active");
+		}, function(){
+			var self = $(this);
+			$(".map__shop_type_" + self.data("type")).myRemoveClass("map__shop_active");
+		});
+	}
 });
